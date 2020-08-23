89dc33ba206b839c7f93ae354ffc6b2ee24b7a86
Jeremy Greer <jex.grizzle@gmail.com>
2020-08-23 10:27:04 -0700

    Lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/explicit-module-boundary-types": 0,
-    complexity: [0, 5],
+    complexity: [1, 5],
     "max-statements": [1, 10],
     "max-depth": [1, 3],
   },
--- src/getModulo.ts
+++ src/getModulo.ts
@@ -1,14 +1,26 @@
 import { SideLabel, TruthyError } from "./types"
 import { error } from "./visualize"
 
+const getLeftNumber = (side: SideLabel, basis: any) => {
+  if (basis === 0) {
+    throw new Error(error(side, "%", basis, TruthyError.ModuloNumberZero))
+  }
+  return basis * 0.5
+}
+
+const getRightNumber = (side: SideLabel, basis: any) => {
+  if (basis === 0) {
+    throw new Error(error(side, "%", basis, TruthyError.ModuloNumberZero))
+  }
+  return basis * 2
+}
+
 const getLeft = (side: SideLabel, basis: any) => {
   const type = typeof basis
   switch (type) {
     case "number":
-      if (basis === 0) {
-        throw new Error(error(side, "%", basis, TruthyError.ModuloNumberZero))
-      }
-      return basis * 0.5
+      return getLeftNumber(side, basis)
+
     case "string": {
       const parsed = parseFloat(basis)
       if (isNaN(parsed)) {
@@ -30,36 +42,35 @@ const getLeft = (side: SideLabel, basis: any) => {
   }
 }
 
-export const getModulo = (side: SideLabel, basis: any): any => {
+const getRight = (side: SideLabel, basis: any) => {
   const type = typeof basis
-
-  if (side === SideLabel.left) {
-    return getLeft(side, basis)
-  } else {
-    switch (type) {
-      case "number":
-        if (basis === 0) {
-          throw new Error(error(side, "%", basis, TruthyError.ModuloNumberZero))
-        }
-        return basis * 2
-      case "string": {
-        const parsed = parseFloat(basis)
-        if (isNaN(parsed)) {
-          throw new Error(
-            error(
-              side,
-              "%",
-              basis,
-              basis
-                ? TruthyError.ModuloRightStringWord
-                : TruthyError.ModuloRightStringEmpty,
-            ) + "\nhttps://www.destroyallsoftware.com/talks/wat",
-          )
-        }
-        return getModulo(side, parsed)
+  switch (type) {
+    case "number":
+      return getRightNumber(side, basis)
+    case "string": {
+      const parsed = parseFloat(basis)
+      if (isNaN(parsed)) {
+        throw new Error(
+          error(
+            side,
+            "%",
+            basis,
+            basis
+              ? TruthyError.ModuloRightStringWord
+              : TruthyError.ModuloRightStringEmpty,
+          ) + "\nhttps://www.destroyallsoftware.com/talks/wat",
+        )
       }
-      default:
-        throw new Error(`unhandled case "${type}"`)
+      return getModulo(side, parsed)
     }
+    default:
+      throw new Error(`unhandled case "${type}"`)
   }
 }
+
+export const getModulo = (side: SideLabel, basis: any): any =>
+  side === SideLabel.left
+    ? //
+      getLeft(side, basis)
+    : //
+      getRight(side, basis)
